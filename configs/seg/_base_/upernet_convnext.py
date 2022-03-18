7dcd12328bea08d316fa1d22ca27c80c8995ef11
mzr1996 <mzr1996@163.com>
2022-03-18 16:17:11 +0800

    Add segmentation config

---
+++ configs/seg/_base_/upernet_convnext.py
@@ -0,0 +1,53 @@
+# import the `models` package of this repository. After importing, our
+# backbones are registered and can be found by the type name.
+custom_imports = dict(imports=['models'], allow_failed_imports=False)
+
+# Replace the path to your own pre-trained model.
+pretrained = 'checkpoints/convnext-base_in1k.pth'
+# As an example, you can also download it from
+# https://download.openmmlab.com/mmclassification/v0/convnext/downstream/convnext-base_3rdparty_32xb128-noema_in1k_20220301-2a0ee547.pth
+
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+
+model = dict(
+    type='EncoderDecoder',
+    pretrained=None,
+    backbone=dict(
+        # Our backbone is registered to mmcls, you can use it from mmdet directly
+        # with the prefix "mmcls."
+        type='mmcls.ConvNeXt',
+        arch='base',
+        out_indices=[0, 1, 2, 3],
+        drop_path_rate=0.4,
+        layer_scale_init_value=1.0,
+        gap_before_final_norm=False,
+        init_cfg=dict(
+            type='Pretrained', checkpoint=pretrained, prefix='backbone.')),
+    decode_head=dict(
+        type='UPerHead',
+        in_channels=[128, 256, 512, 1024],
+        in_index=[0, 1, 2, 3],
+        pool_scales=(1, 2, 3, 6),
+        channels=512,
+        dropout_ratio=0.1,
+        num_classes=19,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+    auxiliary_head=dict(
+        type='FCNHead',
+        in_channels=384,
+        in_index=2,
+        channels=256,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=0.1,
+        num_classes=19,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
